f33fda908877c8bbd2cdaeb6fd7e6ca43592dd82
Maya Oum <107716017+d4vynn@users.noreply.github.com>
2024-02-14 19:23:41 -0500

    update
    worked on javascript

--- page/script.js
+++ page/script.js
@@ -2,16 +2,25 @@ const loginSec = document.getElementById("login-section");
 const loginButton = document.getElementById("login-bttn");
 const loginError = document.getElementById("login-error");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const user = loginSec.username.value;
-    const pass = loginSec.password.value;
+if (loginButton){
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
 
-    if (user == "username123" && pass == "password"){
-        alert("You have successfully logged in.");
-        location.href = "page.html";
-    } else {
-        loginError.style.opacity = 1;
-    }
+        const user = loginSec.username.value;
+        const pass = loginSec.password.value;
 
-})
+        if (user == "user123" && pass == "12345"){
+            alert("Hello Stranger :)");
+            location.href = "page.html";
+        } else {
+            loginError.style.opacity = 1;
+        }
+    });
+}
+
+const changeText = document.getElementById('text');
+const nextButton = document.getElementById('next-bttn');
+
+nextButton.addEventListener("click", (e) => {
+    changeText.innerHTML = "change please";
+}) 
