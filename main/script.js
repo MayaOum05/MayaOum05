31a3db05981ae48622dc08f2d6887d6052b83493
Maya <maya.oum02@gmail.com>
2024-02-27 15:20:41 -0500

    update

--- main/script.js
+++ main/script.js
@@ -10,18 +10,9 @@ if (loginButton){
         const pass = loginSec.password.value;
 
         if (user == "user123" && pass == "12345"){
-            alert("Login Successful!");
             location.href = "page.html";
         } else {
             loginError.style.opacity = 1;
         }
     });
 }
-
-const changeText = document.getElementById('text');
-const nextButton = document.getElementById('next-bttn');
-
-nextButton.addEventListener("click", (e) => {
-    changeText.innerHTML = "I am a self-taught Front-End Web Developer currently working towards my A.A. at Valencia College.";
-
-}) 
