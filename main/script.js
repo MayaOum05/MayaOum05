7891458ddd7d3a6f1043d09cb496ad5f37bae0c0
Maya <maya.oum02@gmail.com>
2024-05-06 23:45:40 -0400

    update

--- main/script.js
+++ main/script.js
@@ -1,20 +1,2 @@
-const loginSec = document.getElementById("login-section");
-const loginButton = document.getElementById("login-bttn");
-const loginError = document.getElementById("login-error");
-
-if (loginButton){
-    loginButton.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        const user = loginSec.username.value;
-        const pass = loginSec.password.value;
-
-        if (user == "User" && pass == "CodingIsFun"){
-            location.href = "page.html";
-        } else {
-            loginError.style.opacity = 1;
-        }
-    });
-}
 
 
